88f10b933e32e7db674673e499aa62bf678be8be
Medsamers <148079354+Medsamers@users.noreply.github.com>
2023-12-01 17:38:07 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = "6974600924:AAH_GrUbHedzdLJvz0UAw4ffUdf7sxy2ijw"
+
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'биткойн': 'BTC',
+    'юань': 'CNY',
+    'тенге': 'KZT'
+}
